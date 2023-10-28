6b5edd6ac1ff1e2cac1eb326f441fa5d19caa7a2
Rashad Mirzayev Private <rashadmirzayevprivate@Rashads-MacBook-Pro.local>
2023-10-27 23:08:28 -0500

    new changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Your App Title</title>
-    <!-- Include your CSS and JavaScript assets here -->
+    <title>Wisher.az - Hədiyyə və Arzuların Ünvanı</title>
 </head>
 <body>
     <header>
@@ -17,7 +16,7 @@
     </nav>
     
     <main>
-        @yield('content') <!-- This is where the content of each page will be inserted -->
+        @yield('content')
     </main>
     
     <footer>
