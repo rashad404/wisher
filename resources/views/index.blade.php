1b019a10a566e500ad4336429fc9fcd4a66a293d
Rashad M <rashad.mirzaDDC.404@gmail.com>
2023-10-28 01:57:51 -0500

    new changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,19 @@
 
 @section('content')
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div class="sm:mx-auto sm:w-full sm:max-w-xl">
         <img class="mx-auto h-10 w-auto" src="{{ asset('images/logo.svg') }}" alt="Wisher.az">
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Hədiyyə və Arzuların Ünvanı!</h2>
+        <h2 class="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Hədiyyə və Arzuların Ünvanı!</h2>
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"> TEZLIKLƏ!</h2>
+
+        <h3 class="mt-10 text-center  leading-9 tracking-tight text-gray-900">
+            <p class="mb-4">Wisher.az xüsusi günləri xatırlamaq və qeyd etmək üçün şəxsi köməkçinizdir. </p>
+            <p class="mb-4">Siz onu ad günləri, yubileylər və ya hər hansı mühüm tarixlərdə avtomatik olaraq sms mesaj və ya elektron poçt göndərmək üçün istifadə edə bilərsiniz və hətta avtomatik hədiyyələr göndərə bilərsiniz. </p>
+            <p class="mb-4">Hədiyyə və mesajlar hər bir şəxsə uyğun fərdiləşdirilə bilinir.</p>
+            <p class="mb-4">Wisher.az -dan həm fərdi şəxslər, həm də şirkətlər istifadə edə bilər.</p>
+            <p class="mb-4">Şirkətlər üçün hədiyyə / təbrik prosesini avtomatikləşdirmək və hər bir işçiyə və ya biznes partnyora dəyərli olduğunu hiss etdirmək daha asan olacaq.</p>
+            <p class="mb-4">Artıq vacib tarixləri unutmaq və ya düzgün hədiyyələr seçməklə bağlı narahatlığınız olmayacaq - Wisher.az hər bir anı həm sizin, həm də sevdikləriniz üçün xüsusi edərək, bütün bunların qayğısına qalır.</p>
+        </h3>
     </div>
 </div>
 @endsection
