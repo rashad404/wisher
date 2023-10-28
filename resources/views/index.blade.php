33959ead250b4ea8bc8b82d707bb5787018e7b63
Rashad M <rashad.mirzaDDC.404@gmail.com>
2023-10-27 23:40:52 -0500

    new changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img class="mx-auto h-10 w-auto" src="{{ asset('images/logo.svg') }}" alt="Wisher.az">
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Hədiyyə və Arzuların Ünvanı!</h2>
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"> TEZLIKLƏ!</h2>
     </div>
 </div>
 @endsection
