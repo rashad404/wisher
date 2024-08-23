6b523749e17341f3694c5061281ba1ee63eaf3c0
Rashad M <rashad.mirzaDDC.404@gmail.com>
2024-08-22 19:02:37 -0500

    calendar etc

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -1,6 +1,6 @@
 <div class="lg:pl-72">
   <div class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-    <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+    <button id="mobileMenuButton" type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
       <span class="sr-only">Open sidebar</span>
       <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -73,5 +73,11 @@
       if (!button.contains(event.target) && !dropdown.contains(event.target)) {
           dropdown.classList.add('hidden');
       }
+  });
+
+    // Mobile menu toggle
+    document.getElementById('mobileMenuButton').addEventListener('click', function() {
+      var sidebar = document.getElementById('mobile-sidebar');
+      sidebar.classList.toggle('hidden');
   });
 </script>
